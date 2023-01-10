05127f367323c80e83db0d32c47c81fc1f37cb59
RedouanMP <113189488+Deersens@users.noreply.github.com>
2023-01-10 12:50:17 +0100

    Update grid.py

--- Representatie/grid.py
+++ Representatie/grid.py
@@ -2,12 +2,10 @@
 class Grid():
     def __init__(self):
         self.grid = []
-        
-    def create_grid(self):
-        """ Generates a board based on a 6x6 grid """
-        for column in range(1, 6):
-            for row in range (1, 6):
-                self.grid[column][row].append("-")
-
+        element = "-"
+        for column in range (1, 7):
+            for row in range (1, 7):
+                self.grid.append(element)
+    
     def __repr__(self):
         return f'{self.grid}'
