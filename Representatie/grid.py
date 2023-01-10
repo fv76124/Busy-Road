e6f6f30bed79b2f68d8ade6be782825df5c53575
fv76124 <fienvisser1@gmail.com>
2023-01-10 11:49:24 +0100

    grid

--- Representatie/grid.py
+++ Representatie/grid.py
@@ -1,5 +1,13 @@
-""" Class for the grid """
+""" creates class for the grid """
 class Grid():
-    pass
+    def __init__(self):
+        self.grid = []
+        
+    def create_grid(self):
+        """ Generates a board based on a 6x6 grid """
+        for column in range(1, 6):
+            for row in range (1, 6):
+                self.grid[column][row].append("-")
 
-    
+    def __repr__(self):
+        return f'{self.grid}'
