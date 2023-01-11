f9e5ca24d5ec1c849a622fd0dcc9bfe27d441094
fv76124 <fienvisser1@gmail.com>
2023-01-11 10:25:35 +0100

    grid

--- Representatie/grid.py
+++ Representatie/grid.py
@@ -3,9 +3,10 @@ class Grid():
     def __init__(self):
         self.grid = []
         element = "-"
-        for column in range (1, 7):
-            for row in range (1, 7):
-                self.grid.append(element)
+        for column in range (6):
+            self.grid.append([])
+            for row in range (6):
+                self.grid[column].append(row)
     
     def __repr__(self):
         return f'{self.grid}'
