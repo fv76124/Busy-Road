8da61f99d6f2f23ff6a9c48bd20fa2cef4f47e26
fv76124 <fienvisser1@gmail.com>
2023-01-11 09:56:23 +0100

    load file

--- Representatie/main.py
+++ Representatie/main.py
@@ -3,8 +3,16 @@ from grid import Grid
 def load_file(filename):
     """ reads the file """
     with open(filename) as file:
+        header = file.readline
         for line in file:
-            pass
+            set = line.split(',')
+            car = set[0]
+            orientation = set[1]
+            column = set[2]
+            row = set[3]
+            length = set[4]
+            
+            
     
 if __name__ == "__main__":
     board = Grid()
